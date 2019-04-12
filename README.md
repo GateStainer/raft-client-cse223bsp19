ba6a597f83cda67f03d5f1076a84b89909a83981
TheNetAdmin <zxwang42@gmail.com>
2019-04-12 00:16:27 -0700

    Client usable.

---
+++ README.md
@@ -0,0 +1,12 @@
+# RAFT Client
+
+## Python packages
+
+- grpcio
+- grpcio-tools
+- click
+
+## Initial build
+
+1. Run `gen_proto.sh` to generate python files from `kvstore.proto`
+2. Run `client.py` to connect server
---
+++ src/client.py
@@ -0,0 +1,22 @@
+import logging
+import grpc
+
+import kvstore_pb2
+import kvstore_pb2_grpc
+
+import click
+
+@click.command()
+@click.option('--address', '-a', type=str, default="localhost:7000")
+def run(address):
+    with grpc.insecure_channel(address) as channel:
+        stub = kvstore_pb2_grpc.KeyValueStoreStub(channel)
+        response = stub.Put(kvstore_pb2.PutRequest(key = "1", value = "100"))
+        print("Client PUT received: " + str(response.ret))
+
+        response = stub.Get(kvstore_pb2.GetRequest(key = "1"))
+        print("Client GET received: " + str(response.value))
+
+if __name__ == "__main__":
+    logging.basicConfig()
+    run()
