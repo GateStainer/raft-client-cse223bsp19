346c45164f0faa374c54648ec94dca10145af8db
michael cheng <mcheng916@gmail.com>
2019-04-14 23:25:14 -0700

    not yet tested, but chaos monkey is implemented

--- src/client.py
+++ src/client.py
@@ -22,19 +22,19 @@ def run(address):
 
         # ChaosMoney matrix
         cmMat = chaosmonkey_pb2.ConnMatrix()
-        row1 = cmMat.rows.add()
-        row1.vals = "0.25 0.25 0.25"
-        row2 = cmMat.rows.add()
-        row2.vals = "0.25 0.25 0.25"
-        row3 = cmMat.rows.add()
-        row3.vals = "0.25 0.25 0.25"
+        for i in range(3):
+            row = cmMat.rows.add()
+            for j in range(3):
+                row.add(float(0.25))
 
         cmstub = chaosmonkey_pb2_grpc.ChaosMonkeyStub(channel)
-        response = cmstub.UploadMatrix(cmMat)
-        print("Client Upload ChaosMonkey Matrix received: " + str(response.ret))
+        addresses = ["localhost:7000", "localhost:7001", "localhost:7002"]
+        for address in addresses:
+            response = cmstub.UploadMatrix(cmMat)
+            print("Client Upload ChaosMonkey Matrix received: " + str(response.ret) + "@: "+address)
 
-        response = cmstub.UpdateValue(chaosmonkey_pb2.MatValue(row = 1, col = 2, val = 0.99))
-        print("Client Update ChaosMonkey Matrix Value received: " + str(response.value))
+            response = cmstub.UpdateValue(chaosmonkey_pb2.MatValue(row = 1, col = 2, val = 0.99))
+            print("Client Update ChaosMonkey Matrix Value received: " + str(response.value) + "@: "+address)
 
 if __name__ == "__main__":
     logging.basicConfig()
