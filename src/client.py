2ca503301a832a6bb9aa44ef836c5b181315bdbd
michael cheng <mcheng916@gmail.com>
2019-04-14 17:57:04 -0700

    Client with chaosmonkey

--- src/client.py
+++ src/client.py
@@ -3,8 +3,11 @@ import grpc
 
 import kvstore_pb2
 import kvstore_pb2_grpc
+import chaosmonkey_pb2
+import chaosmonkey_pb2_grpc
 
 import click
+
 
 @click.command()
 @click.option('--address', '-a', type=str, default="localhost:7000")
@@ -16,6 +19,22 @@ def run(address):
 
         response = stub.Get(kvstore_pb2.GetRequest(key = "1"))
         print("Client GET received: " + str(response.value))
+
+        # ChaosMoney matrix
+        cmMat = chaosmonkey_pb2.ConnMatrix()
+        row1 = cmMat.rows.add()
+        row1.vals = "0.25 0.25 0.25"
+        row2 = cmMat.rows.add()
+        row2.vals = "0.25 0.25 0.25"
+        row3 = cmMat.rows.add()
+        row3.vals = "0.25 0.25 0.25"
+
+        cmstub = chaosmonkey_pb2_grpc.ChaosMonkeyStub(channel)
+        response = cmstub.UploadMatrix(cmMat)
+        print("Client Upload ChaosMonkey Matrix received: " + str(response.ret))
+
+        response = cmstub.UpdateValue(chaosmonkey_pb2.MatValue(row = 1, col = 2, val = 0.99))
+        print("Client Update ChaosMonkey Matrix Value received: " + str(response.value))
 
 if __name__ == "__main__":
     logging.basicConfig()
--- src/kvstore_pb2.py
+++ src/kvstore_pb2.py
@@ -13,8 +13,6 @@ from google.protobuf import symbol_database as _symbol_database
 _sym_db = _symbol_database.Default()
 
 
-
-
 DESCRIPTOR = _descriptor.FileDescriptor(
   name='kvstore.proto',
   package='kvstore',
