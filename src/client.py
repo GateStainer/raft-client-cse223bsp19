c69ae0ac2935ae1b54faff7e536cf06aefca8ef0
michael cheng <mcheng916@gmail.com>
2019-04-15 22:52:28 -0700

    Added comments

--- src/client.py
+++ src/client.py
@@ -15,7 +15,7 @@ def run(address):
 
     with grpc.insecure_channel(address) as channel:
         stub = kvstore_pb2_grpc.KeyValueStoreStub(channel)
-        print("Try to put")
+        # print("Try to put")
         response = stub.Put(kvstore_pb2.PutRequest(key = "1", value = "100"))
         print("Client PUT received: " + str(response.ret))
 
@@ -23,9 +23,8 @@ def run(address):
         print("Client GET received: " + str(response.value))
 
     with grpc.insecure_channel('localhost:7000') as channel:
-        # ChaosMoney matrix
+        # ChaosMoney matrix creation
         cmMat = chaosmonkey_pb2.ConnMatrix()
-        print("cmMat got")
         for i in range(3):
             mat_row = cmMat.rows.add()
             for j in range(3):
@@ -33,10 +32,11 @@ def run(address):
                 # mat_row.vals.add(float(0.25))
 
         cmstub = chaosmonkey_pb2_grpc.ChaosMonkeyStub(channel)
+        # Add more addresses
         addresses = ["localhost:6000"]
         for address in addresses:
             response = cmstub.UploadMatrix(cmMat)
-            print("Client Upload ChaosMonkey Matrix received: " + str(response.ret) + "@: "+address)
+            print("Client Upload ChaosMonkey Matrix received: " + str(response.ret) + ", @: "+address)
 
             response = cmstub.UpdateValue(chaosmonkey_pb2.MatValue(row = 1, col = 2, val = 0.99))
             print("Client Update ChaosMonkey Matrix Value received: " + str(response.ret) + "@: "+address)
