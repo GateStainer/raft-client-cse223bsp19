0c9c609e16b662fcde4ce3a9e1456cd49bbc3c06
PUXI <puxinju@gmail.com>
2019-05-05 17:47:05 -0700

    Add CM to crash/restart one server

--- src/chaos.py
+++ src/chaos.py
@@ -1,7 +1,7 @@
 import logging
 import grpc
 import time
-
+import csv
 import kvstore_pb2
 import kvstore_pb2_grpc
 import chaosmonkey_pb2
@@ -12,24 +12,31 @@ import click
 
 @click.command()
 def run():
-    address = ['35.160.215.250:7000', '34.208.60.253:7000', '54.218.144.35:7000', '52.13.110.148:7000', \
-               '54.188.17.95:7000', '34.222.132.15:7000', '52.33.179.213:7000', '54.212.238.60:7000', \
-               '35.167.51.122:7000', '54.187.20.63:7000']
-    start = time.time()
-    for i in range(100):
-        for a in address:
-            upload(a)
-    print(time.time() - start)
+    address = readAddress("server-list.csv")
+    for a in address:
+        upload(a)
+    update_server(address[5], 5, True)
 
+def readAddress(file):
+    servers = []
+    with open(file, 'r') as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            servers.append(dict(row))
+    address = []
+    for s in servers:
+        address.append(s["address"]+":"+s["port"])
+    return address
 
 def upload(address):
     with grpc.insecure_channel(address) as channel:
         # ChaosMoney matrix creation
         cmMat = chaosmonkey_pb2.ConnMatrix()
-        for i in range(10):
+        server_num = len(address)
+        for i in range(server_num):
             mat_row = cmMat.rows.add()
-            for j in range(10):
-                mat_row.vals.append(float(0.25))
+            for j in range(server_num):
+                mat_row.vals.append(float(0.0))
                 # mat_row.vals.add(float(0.25))
 
         cmstub = chaosmonkey_pb2_grpc.ChaosMonkeyStub(channel)
@@ -37,8 +44,21 @@ def upload(address):
         response = cmstub.UploadMatrix(cmMat)
         print("Client Upload ChaosMonkey Matrix received: " + str(response.ret) + ", @: "+address)
 
-        response = cmstub.UpdateValue(chaosmonkey_pb2.MatValue(row = 1, col = 2, val = 0.99))
-        print("Client Update ChaosMonkey Matrix Value received: " + str(response.ret) + "@: "+address)
+        # response = cmstub.UpdateValue(chaosmonkey_pb2.MatValue(row = 1, col = 2, val = 0.99))
+        # print("Client Update ChaosMonkey Matrix Value received: " + str(response.ret) + "@: "+address)
+
+# Update CM to simulate server crashing or restarting
+def update_server(address, server_index, crash):
+    value = 0.0
+    if crash:
+        value = 1.0
+    with grpc.insecure_channel(address) as channel:
+        cmstub = chaosmonkey_pb2_grpc.ChaosMonkeyStub(channel)
+        for row in range(len(address)):
+            cmstub.UpdateValue(chaosmonkey_pb2.MatValue(row = row, col = server_index, val = value))
+        for col in range(len(address)):
+            cmstub.UpdateValue(chaosmonkey_pb2.MatValue(row = server_index, col = col, val = value))
+
 
 if __name__ == "__main__":
     logging.basicConfig()
